55382ce8b28faaf4ed61afb96fcd649edc06a6b4
Joey <joeypobl@buffalo.edu>
2025-07-25 22:03:01 -0400

    Add base code

--- client/src/renderer/src/MainApp.jsx
+++ client/src/renderer/src/MainApp.jsx
@@ -16,11 +16,24 @@ import { ToggleProvider } from './components/context/UseContext';
 import { useToggle } from './components/context/UseContext';
 import { TourProvider } from './components/context/SettingsTourContext';
 import { HomeProvider } from './components/context/HomeContext';
+import DarkVeil from './components/background/DarkVeil';
+import Galaxy from './components/background/Galaxy';
+import { toggleBackgroundSettings } from './components/context/BackgroundSettingsContext';
+
 
 function MainApp() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
   const [page, setPage] = useState("Home");
+  // const [background, setbackground] = useState("");
   const ref = useRef(null);
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const height = dimensions.height + 100;
+  const {background, setBackground} = toggleBackgroundSettings();
+
 
   const homeClicked = () => {
     setPage('Home')
@@ -40,30 +53,52 @@ function MainApp() {
 
   const {showDock} = useToggle()
 
+  // useEffect(()=>{
+  //   if (background === 'veil'){
+  //       document.body.style.backgroundColor = 'black';
+  //   }else if (background === 'aurora'){
+  //       document.body.style.backgroundColor = '#1a1a1afd';
+  //   }else if (background === 'galaxy'){
+  //       document.body.style.backgroundColor = 'black';
+  //   }
+
+
+  //   const handleResize = () => {
+  //     // console.log(window.innerWidth, window.innerHeight)
+  //     setDimensions({
+  //       width: window.innerWidth,
+  //       height: window.innerHeight,
+  //     });
+  //   };
+
+  //   window.addEventListener('resize', handleResize);
+
+  //   // Clean up the event listener on unmount
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, [])
+
   return (
     <div className='wrapper'>
-      <Aurora
-        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-        blend={0.5}
-        amplitude={1.0}
-        speed={0.5}
-      />
-        {page === 'Home' && 
-          <HomeProvider>
-            <div className='mainApp'>
-              <Youtubers ref={ref}/>
-            </div>           
-          </HomeProvider>
-        }
+        <div className={background === 'galaxy' && `mt-[150px]`}>
+        {/* <div> */}
+          {page === 'Home' && 
+            <HomeProvider>
+              <div className='mainApp '>
+                <Youtubers ref={ref}/>
+              </div>           
+            </HomeProvider>
+          }
 
-        {page === 'History' && <div className='mainApp'><History /></div>}
-        {page === 'Settings' && 
-          <TourProvider>
-            <Settings/> 
-          </TourProvider>
-        }
-        {page === 'Crop' && <Crop/>}
-
+          {page === 'History' && <div className='mainApp'><History /></div>}
+          {page === 'Settings' && 
+            <TourProvider>
+              <Settings/> 
+            </TourProvider>
+          }
+        </div>
+        
       { showDock &&
         <div className='dock-wrapper flex justify-center items-center '>
           <Dock
